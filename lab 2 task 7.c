6984cd923803e72782dc52810e45a38c07781409
sarimahmed-web <sarimahmed2191@gmail.com>
2026-09-06 04:32:16 +0500

    Add files via upload

---
+++ lab 2 task 7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+int age ;
+float height; 
+printf("Enter your height =");
+ scanf("%f",&height);
+ printf("Enter your age =");
+ scanf("%d",&age);
+ printf("you are %d years old\n",age);
+ printf("you height %.3f tall ",height);
+}
